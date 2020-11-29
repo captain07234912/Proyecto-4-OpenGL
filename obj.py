0ce3075ffa394740063ac2648d6c3eacbac271ad
zeta7234Legend <jorgesm595@gmail.com>
2020-11-29 04:04:36 -0600

    Add files via upload

---
+++ obj.py
@@ -0,0 +1,46 @@
+# Carga un archivo OBJ
+
+
+"""
+Universidad del Valle de Guatemala
+Graficas por computadora
+OpenGL
+Jorge Suchite Carnet 15293
+29/11/2020
+
+OBJ pero con OpenGL papu
+
+"""
+class Obj(object):
+    def __init__(self, filename):
+        with open(filename, 'r') as file:
+            self.lines = file.read().splitlines()
+
+        self.vertices = []
+        self.normals = []
+        self.texcoords = []
+        self.faces = []
+
+        self.read()
+
+    def read(self):
+        for line in self.lines:
+            if line:
+                try:
+                    prefix, value = line.split(' ', 1)
+                except:
+                    continue
+
+                if prefix == 'v': # vertices
+                    self.vertices.append(list(map(float,value.split(' '))))
+                elif prefix == 'vn':
+                    self.normals.append(list(map(float,value.split(' '))))
+                elif prefix == 'vt':
+                    self.texcoords.append(list(map(float,value.split(' '))))
+                elif prefix == 'f':
+                    self.faces.append([list(map(int,vert.split('/'))) for vert in value.split(' ')])
+
+
+
+
+
